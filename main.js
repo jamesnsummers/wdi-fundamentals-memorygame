9a7a00ae8c67c106e3ecd58789d7c0c2d166bb48
jamesnsummers <jamesnsummers@gmail.com>
2017-04-23 22:17:54 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+var cards = [
+{
+	rank: "Queen",
+	suit: "Hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "Queen",
+	suit: "Diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "King",
+	suit: "Hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "King",
+	suit: "Diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
+var cardsInPlay = [];
+
+var checkForMatch = function () {
+   this.setAttribute('src', cards[cardId].cardImage)
+   if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+  } else {
+      alert("Sorry, try again.");
+  }
+};
+
+var flipCard = function () {
+  var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	checkForMatch();
+};
+
+var createBoard = function () {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    document.getElementById('cardElement').addEventListener('click', 'flipCard');
+    cardElement.appendChild('game-board');
+    }
+};
+
+createBoard();
